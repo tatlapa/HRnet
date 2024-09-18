62567621c805db58938a0cf67c45bddb8b4c2021
Richard Jérémy <jeremy.rc77@gmail.com>
2024-09-18 23:24:33 +0700

    project finish

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -37,7 +37,7 @@ const Form: React.FC<FormProps> = (props) => {
         street,
         city,
         state,
-        zip,
+        zip: zip !== null ? zip : 0,
       };
       employeeContext.addEmployee(employee);
       notify();
